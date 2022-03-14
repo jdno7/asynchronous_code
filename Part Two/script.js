d704fba89ebce9a770f08fb71340a67fe9031c0c
john hans <jdhans34@gmail.com>
2022-03-13 19:22:48 -0500

    complete

--- Part Two/script.js
+++ Part Two/script.js
@@ -1,16 +1,56 @@
 
-
 const $drawBtn = $('button') 
 
-function clickHandler (evt){
-    $.getJSON(`http://deckofcardsapi.com/api/deck/new/draw/?count=1`)
-    .then(data => {
-        // console.log(data.cards)
-        const card = data.cards[0].image
-        console.log(data.cards[0].image)
-        $('body').append(`<div><img src=${card} alt=""><?div>`)
-    });
-    
+
+async function getCard (){
+    res = await axios.get('http://deckofcardsapi.com/api/deck/new/draw/?count=1')
+    console.log(res.data.cards[0].value, res.data.cards[0].suit)
+}
+async function getTwoCards (){
+    res = await axios.get('http://deckofcardsapi.com/api/deck/new/draw/?count=1')
+    res2 = await axios.get('http://deckofcardsapi.com/api/deck/new/draw/?count=1')
+    console.log(res.data.cards[0].value, res.data.cards[0].suit)
+    console.log(res2.data.cards[0].value, res.data.cards[0].suit)
+}
+
+const deck = {
+    async init () {
+        res = await axios.get('http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        this.deckId = (res.data.deck_id)
+    },
+    async shuffle () {
+        res = await axios.get(`http://deckofcardsapi.com/api/deck/${this.deckId}/shuffle/?remaining=true`)
+        this.remaining = res.data.remaining
+        // console.log(res.data.remaining)
+    },
+    async getCard () {
+        res = await axios.get(`http://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=1`)
+        // console.log(res.data.cards[0].image)
+        return res.data.cards[0].image
+    }
+}
+
+async function createDeck (){
+   await deck.init()
+   deck.shuffle()
+}
+
+
+
+
+async function clickHandler (evt){
+    const card = await deck.getCard()
+    $('body').append(`<div><img src=${card}></div>`)
+    // console.log(deck.remaining)
+    if (deck.remaining === 0) {
+        alert('Game Over, Re-Shuffle?')
+        location.reload()
+    }
+    else {
+        deck.shuffle()
+    }
 }
 
 $drawBtn.on('click', clickHandler)
+
+createDeck()
